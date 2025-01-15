2b7a8f822a0807aa046ac76f0e7d0827c56f8ebf
farizwildan <wildanfariz99@gmail.com>
2025-01-15 21:14:47 +0700

    done

--- main.js
+++ main.js
@@ -13,9 +13,31 @@ function caesarCipherEncrypt(plaintext, key) {
   return ciphertext.toUpperCase();
 }
 
+function caesarCipherDecrypt(ciphertext, key) {
+  var plaintext = "";
+  for (var i = 0; i < ciphertext.length; i++) {
+    var c = ciphertext.charCodeAt(i);
+    if (c >= 65 && c <= 90) {
+      plaintext += String.fromCharCode((c - 65 - key + 26) % 26 + 65);
+    } else if (c >= 97 && c <= 122) {
+      plaintext += String.fromCharCode((c - 97 - key + 26) % 26 + 97);
+    } else {
+      plaintext += ciphertext.charAt(i);
+    }
+  }
+  return plaintext;
+}
+
 function encrypt() {
-  var plaintext = document.getElementById("plaintext").value;
-  var key = parseInt(document.getElementById("key").value);
+  var plaintext = document.getElementById("encrypt-plaintext").value;
+  var key = parseInt(document.getElementById("encrypt-key").value);
   var ciphertext = caesarCipherEncrypt(plaintext, key);
-  document.getElementById("ciphertext").value = ciphertext;
+  document.getElementById("encrypt-ciphertext").value = ciphertext;
+}
+
+function decrypt() {
+  var ciphertext = document.getElementById("decrypt-ciphertext").value;
+  var key = parseInt(document.getElementById("decrypt-key").value);
+  var plaintext = caesarCipherDecrypt(ciphertext, key);
+  document.getElementById("decrypt-plaintext").value = plaintext;
 }
